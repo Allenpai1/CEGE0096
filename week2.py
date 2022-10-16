4881f5649ca8d7ff554a8ac7bc9b21dec8d046e0
HACK PAI <69915548+Allenpai1@users.noreply.github.com>
2022-10-16 15:24:22 +0100

    Exercise 12

--- week2.py
+++ week2.py
@@ -1,6 +1,21 @@
-full_name = input('Please input your full name: ')
+n = int(input('Please enter an integer: '))
 
+while n >= 1 and n <= 7:
+    if n == 1:
+        print('Monday')
+    elif n == 2:
+        print('Tuesday')
+    elif n == 3:
+        print('Wednesday')
+    elif n == 4:
+        print('Thursday')
+    elif n == 5:
+        print('Friday')
+    elif n == 6:
+        print('Saturday')
+    elif n == 7:
+        print('Sunday')
 
-if __name__=='__main__':
-    print(len(full_name))
-    
+    n = int(input('Please enter an integer: '))
+
+print('The provided integer does not correspond to a day!')
